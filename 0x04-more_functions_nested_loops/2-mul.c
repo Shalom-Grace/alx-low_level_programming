6c6ccbdf534323ae48357f1e7bdc510324c4c131
Alola Shalom <alolashalom@yahoo.com>
2022-06-09 21:10:48 -0700

    third commit

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ *mul - Multiply two given integers
+ *@a: First given integer
+ *@b: Second given integer
+ *
+ * Return: Return product of two integers
+ */
+int mul(int a, int b)
+{
+	int ans = a * b;
+
+	return (ans);
+}
