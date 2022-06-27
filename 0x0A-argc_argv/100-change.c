160cfbed7b11245243b9c9008bed4f33249a047f
Shalom-Grace <alolashalom@yahoo.com>
2022-06-27 09:51:38 -0700

    0x0A-argc_argv

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -13,8 +13,7 @@ int main(int argc, char *argv[])
 	int cents, coins = 0;
 
 	if (argc == 2)
-	{
-		cents = atoi(*(argv + 1));
+	{	cents = atoi(*(argv + 1));
 		while (cents > 0)
 		{
 			if (cents % 25 < cents)
